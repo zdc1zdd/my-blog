83c66edb034de7ea95eefd77313d70bf3006dacc
zdd <zdc1zdd@gmail.com>
2026-08-01 18:30:16 +0900

    更新网址为https://zddblog.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://zddblog.com',
 	integrations: [mdx(), sitemap()],
 
 	markdown: {
