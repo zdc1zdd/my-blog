9643533b10aef573e70251debeaaf1fc8d45478f
zdd <zdc1zdd@gmail.com>
2026-08-01 17:12:38 +0900

    初始化博客

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,22 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+
+	markdown: {
+    remarkPlugins: [remarkMath],   // 识别公式语法
+    rehypePlugins: [rehypeKatex],  // 渲染公式
+	shikiConfig: {
+    theme: 'github-dark', // 换成你喜欢的主题名
+  	},
+  	},
+
 	fonts: [
 		{
 			provider: fontProviders.local(),
